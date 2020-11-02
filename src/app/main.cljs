2b06077a5b9515781ad8d63ea3cc6de93e601cfb
przemyslaw.sowinski <psowinski@users.noreply.github.com>
2020-11-02 23:45:17 +0100

    refer js

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,4 +1,21 @@
-(ns app.main)
+(ns app.main
+  (:require [fs :refer (readdirSync)]))
+
 (defn run [& args]
-  (prn "my node app")
-)
+  (prn args)
+  (let [path (first args)
+        dirs (readdirSync path)]
+    (doseq [d dirs]
+      (prn d)))
+
+  ;js/__dirname
+; ;   (let [x (.opendirSync fs)]
+;   (prn (js->clj dirs))
+;   (prn (count dirs))
+;   (prn "przed")
+;   (doseq [i (range 1 6)] (prn i))
+;   (prn "po")
+  ;(for [d (range 5)]
+  ;  (println "taki jest"))
+
+  0)
