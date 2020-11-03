14cd88a7e304d04f5f6f79e92700c67fce532919
przemyslaw.sowinski <psowinski@users.noreply.github.com>
2020-11-03 21:35:05 +0100

    fighting with cheerio & js

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -3,9 +3,31 @@
             [path]
             [cheerio]))
 
+; (defn html-collection-to-vec
+;   "Converts nodelist to (not lazy) seq."
+;   [hc]
+;   (into [] (for [k (range (aget hc "length"))]
+;              (aget hc k))))
+
+
+
+(defn testsome [doc]
+  (def ^js/Cheerio x (doc "h1"))
+  ;(def z (.html x))
+  ;(-> z js->clj prn))
+  ;(prn (cheerio/html x))
+  (prn (.eq x 1))
+  (prn (.-length x))
+  ;(prn (. x get 0))
+  ;(prn (html-collection-to-vec x))
+  ;; (doseq [it x]
+  ;;   (prn it))
+)
+
 (defn scan [content]
   (let [doc (cheerio/load content)]
-    (prn (doc "h3"))
+    ;(prn (doc "h1"))
+     (isRecipe doc)
     ))
 
 (defn readRecipes [path book]
