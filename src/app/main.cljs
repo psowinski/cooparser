50dfb8920c36f2fad07a1948dd0f362126ef025c
przemyslaw.sowinski <psowinski@users.noreply.github.com>
2020-11-03 19:04:37 +0100

    load html with cheerio

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,21 +1,31 @@
 (ns app.main
-  (:require [fs :refer (readdirSync)]))
+  (:require [fs :refer (readdirSync readFileSync)]
+            [path]
+            [cheerio]))
+
+(defn scan [content]
+  (let [doc (cheerio/load content)]
+    (prn (doc "h3"))
+    ))
+
+(defn readRecipes [path book]
+  (let [recipes (readdirSync path)]
+    (for [recipe recipes]
+      {:book book
+       :file (path/resolve path recipe)})))
+
+(defn readBooks [path]
+  (let [books (readdirSync path)]
+    (flatten (for [book books]
+               (readRecipes (path/resolve path book) book)))))
+
+(defn procRecipe [recipe]
+  (let [content (readFileSync (:file recipe))]
+    (scan content)))
 
 (defn run [& args]
-  (prn args)
-  (let [path (first args)
-        dirs (readdirSync path)]
-    (doseq [d dirs]
-      (prn d)))
-
+  (let [path (first args)]
+    (doseq [x (take 1 (readBooks path))]
+      (procRecipe x)))
   ;js/__dirname
-; ;   (let [x (.opendirSync fs)]
-;   (prn (js->clj dirs))
-;   (prn (count dirs))
-;   (prn "przed")
-;   (doseq [i (range 1 6)] (prn i))
-;   (prn "po")
-  ;(for [d (range 5)]
-  ;  (println "taki jest"))
-
-  0)
+  )
