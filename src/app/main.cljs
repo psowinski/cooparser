459d6f830194e0449cced26483ad58b112175223
przemyslaw.sowinski <psowinski@users.noreply.github.com>
2020-11-05 18:31:29 +0100

    parse recipes

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -4,7 +4,6 @@
             [cheerio]
             [clojure.string :as str]))
 
-
 (defn cheerio->array [^js/Cheerio coll]
   (into [] (for [idx (range (.-length coll))]
              (.eq coll idx))))
@@ -18,66 +17,102 @@
     (> cnt 0)))
 
 (defn get-icons [doc]
-  (let [^js/Cheerio icons (doc "div[class='qv-info-icons'] > div > p")]
-    {:preperation (get-text (.eq icons 0))
-     :total (get-text (.eq icons 1))
-     :portion (get-text (.eq icons 2))
-     :dificulty (get-text (.eq icons 3))
-     :favourite (get-text (.eq icons 4))}))
+  (let [^js/Cheerio icons (->> (doc "div[class='qv-info-icons'] > div > p")
+                              cheerio->array
+                              (take 5)
+                              (map get-text))]
+    {:preperation (nth icons 0)
+     :total (nth icons 1)
+     :portion (nth icons 2)
+     :dificulty (nth icons 3)
+     :favourite (nth icons 4)}))
+
+(defn get-nutritional [doc]
+  (let [^js/Cheerio section (doc "#qv-nutritional-section > div > div")
+        ^js/Cheerio perunit (-> (.find section "p") .first get-text)
+        ^js/Cheerio vlaues (->> (.find section "div[class='nutritional-values'] > div > span")
+                                cheerio->array
+                                (take 4)
+                                (map get-text))]
+    {:per perunit
+     :energy (nth vlaues 0)
+     :protein (nth vlaues 1)
+     :carb (nth vlaues 2)
+     :fat (nth vlaues 3)}))
 
 (defn get-title [doc]
   (-> (doc "h1[class='qv-recipe-head'] > span") .first get-text))
 
-(defn recognize-icons [doc span]
-  (prn (.find span "strong"))
-  span)
+(defn recognize-symbol-name [name]
+  (case name
+    "icon-dough-mode" "wyrabianie ciasta"
+    "icon-reverse" "obroty wsteczne"
+    "icon-stirring" "mieszanie"
+    (throw (str "NIEZNANY SYMBOL: " name)))
+  )
 
-(defn get-ingridient-rows [doc item]
-  (->> (doc "span" item)
+(defn fill-symbol-text [symbol name]
+  (.text symbol (str "(" (recognize-symbol-name name) ")"))
+  symbol)
+
+(defn convert-symbol [symbol]
+    (let [class (.attr ^js/Cheerio symbol "class")
+          name (subs class 5)]
+      (fill-symbol-text symbol name)))
+
+(defn recognize-symbols [span]
+  (let [icons (cheerio->array (.find span ".icon"))]
+    (doseq [icon icons]
+      (convert-symbol icon))
+    span))
+
+(defn get-ingridient-row [row]
+  (->> (.find row "span")
        cheerio->array
        (map get-text)))
 
-(defn get-preperation-rows [doc item]
-  (->> (doc "span" item)
+(defn get-preperation-row [row]
+  (->> (.find row "span")
        cheerio->array
-       (map (partial recognize-icons doc))
+       (map recognize-symbols)
        (map get-text)))
 
-(defn get-ingridients-list [doc items]
-  (map (partial get-ingridient-rows doc) items))
+(defn get-ingridients-list [rows]
+  (map get-ingridient-row rows))
 
-(defn get-preperation-list [doc items]
-  (map (partial get-preperation-rows doc) items))
+(defn get-preperation-list [rows]
+  (map get-preperation-row rows))
 
-(defn get-group-name [doc group]
-  (-> (doc "h4" group) get-text))
+(defn get-group-name [group]
+  (-> (.find group "h4") get-text))
 
-(defn get-ingridients-group [doc group]
-  (let [ingridients (cheerio->array (doc "ul > li" group))]
-    {:name (get-group-name doc group)
-     :list [] ;(get-ingridients-list doc ingridients)
+(defn get-ingridients-group [group]
+  (let [ingridients (cheerio->array (.find group "ul > li"))]
+    {:name (get-group-name group)
+     :list (get-ingridients-list ingridients)
      }))
 
-(defn get-preperation-group [doc group]
-  (let [steps (cheerio->array (doc "ol > li" group))]
-    {:name (get-group-name doc group)
-     :list [] ;(get-preperation-list doc steps)
+(defn get-preperation-group [group]
+  (let [steps (cheerio->array (.find group "ol > li"))]
+    {:name (get-group-name group)
+     :list (get-preperation-list steps)
      }
     ))
 
 (defn get-ingridients [doc]
   (let [groups (cheerio->array (doc "#qv-ingredient-section > div > div > div"))]
-    (map (partial get-ingridients-group doc) groups)))
+    (map get-ingridients-group groups)))
 
 (defn get-preperation [doc]
   (let [groups (cheerio->array (doc "#qv-preparation-section > div > div[class='recipe-step-groups']"))]
-    (map (partial get-preperation-group doc) groups)))
+    (map get-preperation-group groups)))
 
 (defn scanRecipe [doc]
-  (-> {:name (get-title doc)
-       :summary (get-icons doc)
-       :ingridients (get-ingridients doc)
-       :preperation (get-preperation doc)} prn))
+  (into [] (-> {:name (get-title doc)
+                :summary (get-icons doc)
+                :ingridients (get-ingridients doc)
+                :preperation (get-preperation doc)
+                :nutritional (get-nutritional doc)})))
 
 (defn scan [content]
   (let [doc (cheerio/load content)]
@@ -97,11 +132,15 @@
 
 (defn procRecipe [recipe]
   (let [content (readFileSync (:file recipe))]
-    (scan content)))
+    (prn (scan content))))
+
+(defn show-progress [idx cnt]
+  (if (= (rem (dec idx) 10) 0) (prn (str "Processing " idx "/" cnt)) ()))
 
 (defn run [& args]
-  (let [path (first args)]
-    (doseq [x (take 5 (readBooks path))]
-      (procRecipe x)))
-  ;js/__dirname
-  )
+  (let [path (first args)
+        recipes (map-indexed vector (readBooks path))
+        cnt (count recipes)]
+    (doseq [[idx recipe] (take 2 recipes)]
+      (show-progress idx cnt)
+      (procRecipe recipe))))
